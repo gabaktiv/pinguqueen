6cf5febc0ec9b05dc82bcf9492f061ff36d2e81e
gabriel <gabriel.bieniek@stud.hn.de>
2026-07-17 09:10:40 +0200

    deletion of sandbox.cpp and useless documentation

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 #include "file-handling/ArtIndexBuilder.hpp"
 #include "adaptive-radix-trie/adaptive-radix-trie.hpp"
 
-int main(int argc, char *argv[]) {
+int main( [[maybe_unused]]int argc, [[maybe_unused]]char *argv[] ) {
 
     pinguqueen::file::ArtIndexBuilder builder{};
     auto& trie = builder.art();
